c1821c2ca77c58c8158f02c4d4b5723d506f76c3
Marie <marie.parison@coding-academy.fr>
2018-04-05 16:49:16 +0200

    Router + Dispatcher

--- App/Src/Router.php
+++ App/Src/Router.php
@@ -2,8 +2,26 @@
 
 namespace App\Src;
 
-class Router {
-    static function test() {
-        var_dump("test");
+use App\Config\Configuration;
+use App\Src\Request;
+
+class Router
+{
+    //TODO créer constantes rights
+    const RULES  = array(
+        "{articles/edit/(?P<id>\d+)}" => ["ArticlesController::editAction", 2],
+        "{articles/(?P<id>\d+)}" => ["ArticlesController::render", 0],
+        "{articles}" => ["ArticlesController::render", 0],
+        "" => ["ArticlesController::render", 0],
+    );
+
+    static public function match(Request $request)
+    {
+        foreach(self::RULES as $pattern => $method){
+            if(preg_match($pattern, $path, $params)){
+                $request->setParams($params);
+                return $method;
+            }
+        }
     }
 }
