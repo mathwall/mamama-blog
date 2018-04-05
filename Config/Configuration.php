e689c90ec85a529ef6dbe158bf77be4c33485d07
Math <mathilde.wallon@coding-academy.fr>
2018-04-05 09:48:04 +0200

    DB et config

---
+++ Config/Configuration.php
@@ -0,0 +1,8 @@
+<?php
+
+class Configuration {
+    const DBNAME = 'a_creer';
+    const HOST = 'localhost';
+    const USER = 'root';
+    const MDP = '';
+}
---
+++ Config/Db.php
@@ -0,0 +1,36 @@
+<?php
+require_once 'Configuration.php';
+class Db
+{
+    private static $_pdo = null;
+
+    public static function getDb()
+    {
+        if (is_null(self::$_pdo)) {
+            try {
+                self::$_pdo = new PDO('mysql:dbname=' . Configuration::DBNAME . ';host=' . Configuration::HOST, Configuration::USER, Configuration::MDP);
+            } catch (PDOException $e) {
+                die("PDO ERROR: " . $e->getMessage());
+            }
+        }
+        return self::$_pdo;
+    }
+
+    public static function query($query, $params = null, $fetchall = true)
+    {
+        $pdo = self::getDb();
+        if ($params == null) {
+            $prepared = $pdo->query($query);
+        } else {
+            $prepared = $pdo->prepare($query);
+            $prepared->execute($params);
+        }
+        if ($fetchall == true) {
+            $result = $prepared->fetchAll(PDO::FETCH_ASSOC);
+        } else {
+            $result = $prepared->fetch(PDO::FETCH_ASSOC);
+        }
+        return $result;
+    }
+
+}
