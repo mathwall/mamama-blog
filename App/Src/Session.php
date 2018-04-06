b7f78cee6e6aac90961b7a34278f4c66c6e3dbe5
Denis Ma <madenis86@gmail.com>
2018-04-06 18:55:42 +0200

    session done

--- App/Src/Session.php
+++ App/Src/Session.php
@@ -2,22 +2,58 @@
 
 namespace App\Src;
 
-class Session{
+class Session {
 
-    static public function write(){
-        
+    static public function write($key_string, $value){
+        $keys = explode('.', $key_string);
+        if (count($keys) < 2) {
+            $_SESSION[$keys[0]] = $value;
+            return;
+        }
+
+        $dest = &$_SESSION;
+        for($i = 0; $i < count($keys); $i++) {
+            if (isset($dest[$keys[$i]])) {
+                if (!is_array($dest[$keys[$i]])) {
+                    $dest[$keys[$i]] = [];
+                }
+            } else {
+                $dest[$keys[$i]] = [];
+            }
+            $dest = &$dest[$keys[$i]];
+        }
+        $dest = $value;
+    }
+
+    static public function read($key_string){
+        $keys = explode('.', $key_string);
+        if (count($keys) < 2) {
+            return $_SESSION[$keys[0]];
+        }
+
+        $dest = &$_SESSION;
+        for($i = 0; $i < count($keys); $i++) {
+            if (isset($dest[$keys[$i]])) {
+                if (!is_array($dest[$keys[$i]])) {
+                    if ($i !== count($keys)-1) {
+                        return null;
+                    }
+                }
+            } else {
+                return null;
+            }
+            $dest = &$dest[$keys[$i]];
+        }
+        return $dest;
     }
 
     static public function load(){
-        
-    }
-
-    static public function read(){
-
+        session_start();
     }
 
     static public function destroy(){
-        
+        session_unset();
+        session_destroy();
+        session_reset();
     }
-
 }
